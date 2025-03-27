500353b7308c317ec7642b4fd6dd03e39200e250
reyer3 <56456442+reyer3@users.noreply.github.com>
2025-03-26 23:50:39 -0500

    Crear punto de entrada del frontend

---
+++ frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
+import { AuthProvider } from './context/AuthContext'
+import { TasksProvider } from './context/TasksContext'
+import { ToastProvider } from './context/ToastContext'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ToastProvider>
+        <AuthProvider>
+          <TasksProvider>
+            <App />
+          </TasksProvider>
+        </AuthProvider>
+      </ToastProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
